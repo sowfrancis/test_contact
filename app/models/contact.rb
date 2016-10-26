ed96807544df24f52f11bc34e6f16118563c4fe0
sowfrancis <sow.francis@gmail.com>
2016-10-26 12:52:18 +0200

    validation prenom nom email

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,5 @@
 class Contact < ActiveRecord::Base
+	validates_uniqueness_of :firstname, :lastname, :email
+	validates_length_of :firstname, :lastname, minimum: 3
+	validates_format_of :email, :with => /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, :on => :create
 end
