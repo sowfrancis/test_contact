0846c94539b9c8233518c5c58ce2394fa86404f3
sowfrancis <sow.francis@gmail.com>
2016-10-27 01:15:34 +0200

    ajout methode controller et model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,18 @@
 class Contact < ActiveRecord::Base
+	require 'csv'
+
+	def self.import(file)
+		CSV.foreach(file.path, headers: true) do |row|
+			contact_hash = row.to_hash
+			contact = where(firstname: contact_hash["firstname"])
+			contact = where(lastname: contact_hash["lastname"])
+			contact = where(email: contact_hash["email"])
+			if contact.count == 1
+				puts "already exist"
+			else
+				contact.create!(contact_hash)
+			end 
+		end
+	end
+
 end
